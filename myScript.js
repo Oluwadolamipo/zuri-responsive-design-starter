c897cf91f5fd84e6ef1651977df98d88af6aea05
Oluwadolamipo <dlapsie@gmail.com>
2021-04-11 14:31:06 +0100

    4th commit

--- myScript.js
+++ myScript.js
@@ -1,11 +1,6 @@
 const toggleButton = document.querySelector(".togglebutton"); //Selects the class div with textcontent 'togglebutton'
-//const navbarlinks = document.querySelector('.navbarlinks'); //nav class //
 
 
 
-const clickfunction = toggleButton.addEventlistener('click', event => {
-    toggleButton.innerHTML = `I was clicked ${event.details} times `})
-   
-//navbarlinks.addEventListener('click', ()=>{
-    //navbarlinks.classList.toggle('active')
- //   Navbarlinks.textContent = 'close' ? 'navbarlinks' : 'close' }
+const clickfunction= toggleButton.addEventlistener('click', event => {
+ toggleButton.innerHTML = `I was clicked ${event.details} times `)
